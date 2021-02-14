9c9826ff4ea3c459a6ad41e858dd2edd9ff92839
‘Juliano <julianoperins@gmail.com>
2021-02-14 16:29:17 -0500

    made the first api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div>
-      <h1>Hi there!</h1>
-    </div>
-  );
-}
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+
+  const dictionaryApi = async () => {
+    try {
+      const data = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/plane`
+      );
+      setMeanings(data.data);
+      console.log(meanings);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    dictionaryApi();
+  }, []);
+
+  return <div></div>;
+};
 
 export default App;
