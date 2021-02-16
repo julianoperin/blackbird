5e633697dfa7351225792d7d4a9b50d339c4f800
‘Juliano <julianoperins@gmail.com>
2021-02-16 11:15:41 -0500

    Created the data file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,39 @@
 import React from "react";
 import "./Header.css";
 
+import {
+  TextField,
+  ThemeProvider,
+  createMuiTheme,
+  MenuItem,
+} from "@material-ui/core";
+
 const Header = () => {
+  const darkTheme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
   return (
     <div className="header">
       <span className="title">Dictionary</span>
-      <div className="inputs">hello</div>
+      <div className="inputs">
+        <ThemeProvider theme={darkTheme}>
+          <TextField id="standard-basic" label="Standard" />
+          <TextField
+            id="standard-select-currency"
+            select
+            label="Select"
+            helperText="Please select your currency"
+          >
+            <MenuItem>english</MenuItem>
+          </TextField>
+        </ThemeProvider>
+      </div>
     </div>
   );
 };
